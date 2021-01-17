52a182bed9f61937c2eb6b331b80d9612bca3c26
Mattia <hexer504@gmail.com>
2021-01-17 12:59:43 +0100

    Migrate SourcePawnFileType to Kotlin

---
+++ src/main/kotlin/org/idea_sp/SourcePawnFileType.kt
@@ -0,0 +1,15 @@
+package org.idea_sp
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+object SourcePawnFileType : LanguageFileType(SourcePawnLanguage) {
+
+    override fun getName(): String = "SourcePawn file"
+
+    override fun getDescription(): String = "SourcePawn language file"
+
+    override fun getDefaultExtension(): String = "sp"
+
+    override fun getIcon(): Icon = SourcePawnIcons.FILE
+}
