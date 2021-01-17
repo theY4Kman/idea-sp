63c9557555fad1bc91d59aedcd5b819320835d20
Mattia <hexer504@gmail.com>
2021-01-17 13:32:03 +0100

    Update kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,18 +12,9 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testCompile("junit", "junit", "4.12")
-}
-
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     version = "2020.3.1"
-}
-tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    changeNotes("""
-      Add change notes here.<br>
-      <em>most HTML tags may be used</em>""")
 }
 
 sourceSets.getByName("main") {
