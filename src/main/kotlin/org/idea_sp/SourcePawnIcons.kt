310b7b121681d766564ccd2dd7b5c570a9e513a9
Mattia <hexer504@gmail.com>
2021-01-17 13:23:36 +0100

    Move icons to resources/icons

--- src/main/kotlin/org/idea_sp/SourcePawnIcons.kt
+++ src/main/kotlin/org/idea_sp/SourcePawnIcons.kt
@@ -3,5 +3,5 @@ package org.idea_sp
 import com.intellij.openapi.util.IconLoader
 
 object SourcePawnIcons {
-    val FILE = IconLoader.getIcon("/org/idea_sp/icons/hl2.png")
+    val FILE = IconLoader.getIcon("/icons/hl2.png")
 }
