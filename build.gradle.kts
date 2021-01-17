9c7c5f273eb653370e9d23baaedd00e75824941e
Mattia <hexer504@gmail.com>
2021-01-17 12:07:16 +0100

    Implement gradle

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.6.5"
+    java
+}
+
+group = "org.idea_sp"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.3.1"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
+
+sourceSets.getByName("main") {
+    java.srcDir("src/main/java")
+    java.srcDir("src/main/kotlin")
+    java.srcDir("src/main/grammar")
+    java.srcDir("src/main/gen")
+}
