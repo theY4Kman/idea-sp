16eac76e78f5902a5a15c57546fa1f5729b22593
Zach "theY4Kman" Kanzler <they4kman@gmail.com>
2021-02-02 03:42:18 -0500

    feat(editor): expand syntax highlighting

--- src/main/kotlin/org/idea_sp/SourcePawnLexerAdapter.kt
+++ src/main/kotlin/org/idea_sp/SourcePawnLexerAdapter.kt
@@ -1,7 +1,5 @@
 package org.idea_sp
 
 import com.intellij.lexer.FlexAdapter
-import org.idea_sp._SourcePawnLexer
-import java.io.Reader
 
 class SourcePawnLexerAdapter : FlexAdapter(_SourcePawnLexer(null))
--- src/main/kotlin/org/idea_sp/SourcePawnSyntaxHighlighter.kt
+++ src/main/kotlin/org/idea_sp/SourcePawnSyntaxHighlighter.kt
@@ -6,37 +6,60 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.psi.tree.IElementType
-import org.idea_sp.SourcePawnTokenTypeSets.Companion.COMMENTS
-import org.idea_sp.SourcePawnTokenTypeSets.Companion.KEYWORDS
 import org.idea_sp.psi.SourcePawnTypes
 
 class SourcePawnSyntaxHighlighter : SyntaxHighlighterBase() {
     companion object {
-        val OPERATOR = TextAttributesKey.createTextAttributesKey(
-            "SOURCE_PAWN_ASSIGN",
-            DefaultLanguageHighlighterColors.OPERATION_SIGN
-        )
-        val LITERAL =
-            TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_CONSTANT", DefaultLanguageHighlighterColors.CONSTANT)
-        val RESERVED =
-            TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_RESERVED", DefaultLanguageHighlighterColors.KEYWORD)
-        val COMMENT = TextAttributesKey.createTextAttributesKey(
-            "SOURCE_PAWN_COMMENT",
-            DefaultLanguageHighlighterColors.LINE_COMMENT
-        )
+        val BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT)
+        val LINE_COMMENT = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+        val DOC_COMMENT = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_DOC_COMMENT", DefaultLanguageHighlighterColors.DOC_COMMENT)
+        val TYPE = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_TYPE", DefaultLanguageHighlighterColors.CLASS_REFERENCE)
+        val IDENTIFIER = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER)
+        val NUMBER = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_NUMBER", DefaultLanguageHighlighterColors.NUMBER)
+        val KEYWORD = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD)
+        val STRING = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_STRING", DefaultLanguageHighlighterColors.STRING)
+        val OPERATOR = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_OPERATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
+        val BRACES = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_BRACES", DefaultLanguageHighlighterColors.BRACES)
+        val DOT = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_DOT", DefaultLanguageHighlighterColors.DOT)
+        val SEMICOLON = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_SEMICOLON", DefaultLanguageHighlighterColors.SEMICOLON)
+        val COMMA = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_COMMA", DefaultLanguageHighlighterColors.COMMA)
+        val PARENTHESES = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES)
+        val BRACKETS = TextAttributesKey.createTextAttributesKey("SOURCE_PAWN_BRACKETS", DefaultLanguageHighlighterColors.BRACKETS)
 
         private val ATTRIBUTES: Map<IElementType, TextAttributesKey> = hashMapOf(
-            SourcePawnTypes.EQ to OPERATOR,
-            SourcePawnTypes.LITERAL to LITERAL,
+            SourcePawnTypes.BLOCK_COMMENT to BLOCK_COMMENT,
+            SourcePawnTypes.DOC_COMMENT to DOC_COMMENT,
+            SourcePawnTypes.LINE_COMMENT to LINE_COMMENT,
+            SourcePawnTypes.PREPROCESSOR_COMMENT to LINE_COMMENT,
+
+            SourcePawnTypes.LBRACE to BRACES,
+            SourcePawnTypes.RBRACE to BRACES,
+            SourcePawnTypes.DOT to DOT,
+            SourcePawnTypes.SEMICOLON to SEMICOLON,
+            SourcePawnTypes.COMMA to COMMA,
+            SourcePawnTypes.LPAREN to PARENTHESES,
+            SourcePawnTypes.RPAREN to PARENTHESES,
+            SourcePawnTypes.LBRACKET to BRACKETS,
+            SourcePawnTypes.RBRACKET to BRACKETS,
+
+            SourcePawnTypes.SYMBOL to IDENTIFIER,
+
+            SourcePawnTypes.TYPE_EXPRESSION to TYPE,
+
+            SourcePawnTypes.NUMBER to NUMBER,
+            SourcePawnTypes.RATIONAL to NUMBER,
+            SourcePawnTypes.INTEGER to NUMBER,
         )
 
         init {
-            fillMap(ATTRIBUTES, KEYWORDS, RESERVED);
-            fillMap(ATTRIBUTES, COMMENTS, COMMENT);
+            fillMap(ATTRIBUTES, SourcePawnTokenTypeSets.KEYWORDS, KEYWORD);
+            fillMap(ATTRIBUTES, SourcePawnTokenTypeSets.OPERATORS, OPERATOR);
+            fillMap(ATTRIBUTES, SourcePawnTokenTypeSets.STRINGS, STRING);
         }
     }
 
     override fun getHighlightingLexer(): Lexer = FlexAdapter(_SourcePawnLexer(null))
 
-    override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> = pack(ATTRIBUTES[tokenType])
+    override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> =
+        pack(ATTRIBUTES[tokenType])
 }
